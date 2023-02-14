0e9c698ea1c6105837a4def6eb65bdaa36ea9f9b
GET MOUSSED <mofanafoussa@gmail.com>
2023-02-14 15:55:42 +0100

    justify-center

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
         status, statusListener, playWithVoice
     } from "./stores/game.ts";
     import ColorButton from "./lib/ColorButton.svelte";
-    import {onDestroy, onMount} from "svelte";
+    import {onDestroy} from "svelte";
     import {requestNotifPerm} from "./stores/notifications.ts";
 
     const unsubAnswer = colorPressedByPlayer.subscribe(answerListener);
@@ -28,7 +28,7 @@
 </script>
 
 
-<div class="grid grid-rows-none grid-cols-[1fr_3fr] h-screen w-screen bg-gray-800 text-white items-center">
+<div class="grid grid-rows-none grid-cols-[1fr_3fr] h-screen w-screen bg-gray-800 text-white items-center justify-center">
         <div class="w-full flex flex-col items-center justify-center space-y-4">
             <ScoreBoard/>
             <button class="bg-teal-700 text-sm md:text-lg md:text-2xl px-4 py-2 rounded-xl" on:click={playWithVoice}>Play
